40d27eb184163a928fd224f14ebed054bcf3a44e
MichaelWongK <michael.won007@gmail.com>
2020-07-25 13:07:55 +0800

    feat: 安装element-ui、axios && 页面路由 && 登陆页面 -token状态同步-全局axios拦截

---
+++ src/axios.js
@@ -0,0 +1,43 @@
+import axios from 'axios'
+import Element from 'element-ui'
+import router from './router'
+import store from './store'
+
+axios.defaults.baseURL="http://localhost:8088"
+
+// 前置拦截
+axios.interceptors.request.use(config => {
+    return config
+})
+
+// 后置拦截
+axios.interceptors.response.use(response => {
+        let res = response.data
+
+        console.log(77777777777777)
+        console.log(res)
+
+        if (res.code == 200) {
+            return response
+        } else {
+            Element.Message.error(res.msg, {duration : 3 * 1000});
+            return Promise.reject(res.msg)
+        }
+    },
+    error => {
+        console.log(error)
+
+        if (error.response.data) {
+            error.message = error.response.data.msg
+        }
+
+        if (error.response.status === 401) {
+            store.commit("REMOVE_INFO")
+            router.push("/login")
+        } 
+
+        Element.Message.error(error.message, {duration : 3 * 1000});
+        return Promise.reject(error)
+        
+    }
+)
---
+++ src/views/BlogEdit.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      blogEdit
+  </div>
+</template>
+
+<script>
+export default {
+    name: "BlogEdit.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
